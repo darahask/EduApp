84a8f4e045194e70da782c3dde3114f1810d9350
darahask <darahask12@gmail.com>
2020-08-27 10:41:28 +0530

    Start Project

--- app/src/main/java/com/example/eduapp/MainActivity.java
+++ app/src/main/java/com/example/eduapp/MainActivity.java
@@ -10,9 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("darahas");
-        System.out.println("bhavana");
-        System.out.println("vaishnavi");
-        // this is surya's message about the merge problem.
     }
 }
